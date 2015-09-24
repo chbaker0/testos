854e7e95cad67a689619df242ded5977ef3a8b9b
Collin Baker <chbaker0@gmail.com>
2015-09-24 02:55:55 -0400

    Started IDT code

--- kernel.c
+++ kernel.c
@@ -3,8 +3,30 @@
 #include "port.h"
 #include "console.h"
 
+struct idtr
+{
+	uint16_t limit;
+	uint32_t base;
+};
+
+struct idt_entry
+{
+	uint16_t offset_0_15;
+	uint16_t selector;
+	uint8_t zero;
+	uint8_t type;
+	uint16_t offset_16_31;
+};
+
+idt_entry idt_entries[256];
+
+#define IDT_32_INT_GATE 14u
+#define IDT_32_TRAP_GATE 15u
+
 void kmain()
 {
+	
+	
 	console_init();
 	
 	console_write_line("Hello world from a kernel!");

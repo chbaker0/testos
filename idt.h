0dff6e855d9fe8655ef5df85c774942ab79a48c2
Collin Baker <chbaker0@gmail.com>
2015-09-28 21:58:40 -0400

    Fixed IDT definition

--- idt.h
+++ idt.h
@@ -1,19 +1,22 @@
 #ifndef _IDT_H_INCLUDED_
 #define _IDT_H_INCLUDED_
 
-struct idt_entry
+struct __attribute__ ((__packed__)) idt_entry
 {
-	uint16_t offset_0_15;
-	uint16_t selector;
-	uint8_t zero;
-	uint8_t type;
-	uint16_t offset_16_31;
+    unsigned long long offset_0_15 : 16;
+	unsigned long long selector : 16;
+	unsigned long long zero : 8; // ???
+	unsigned long long type : 4;
+	unsigned long long storage_segment : 1;
+	unsigned long long priv_level : 2;
+	unsigned long long present : 1;
+	unsigned long long offset_16_31 : 16;
 };
 
-#define IDT_32_TASK_GATE 5u
-#define IDT_16_INT_GATE 6u
-#define IDT_16_TRAP_GATE 7u
-#define IDT_32_INT_GATE 14u
-#define IDT_32_TRAP_GATE 15u
+#define IDT_TYPE_32_TASK_GATE 5u
+#define IDT_TYPE_16_INT_GATE 6u
+#define IDT_TYPE_16_TRAP_GATE 7u
+#define IDT_TYPE_32_INT_GATE 14u
+#define IDT_TYPE_32_TRAP_GATE 15u
 
 #endif // _IDT_H_INCLUDED_

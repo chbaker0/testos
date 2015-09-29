47af24cb0b5be2ceff3b1aa17b66f86cfc4308c0
Collin Baker <chbaker0@gmail.com>
2015-09-28 22:00:10 -0400

    Made idt.h look more like gdt.h (for uniformity)

--- idt.h
+++ idt.h
@@ -1,6 +1,12 @@
 #ifndef _IDT_H_INCLUDED_
 #define _IDT_H_INCLUDED_
 
+#define IDT_ENTRY_TYPE_32_TASK_GATE 5u
+#define IDT_ENTRY_TYPE_16_INT_GATE 6u
+#define IDT_ENTRY_TYPE_16_TRAP_GATE 7u
+#define IDT_ENTRY_TYPE_32_INT_GATE 14u
+#define IDT_ENTRY_TYPE_32_TRAP_GATE 15u
+
 struct __attribute__ ((__packed__)) idt_entry
 {
     unsigned long long offset_0_15 : 16;
@@ -12,11 +18,5 @@ struct __attribute__ ((__packed__)) idt_entry
 	unsigned long long present : 1;
 	unsigned long long offset_16_31 : 16;
 };
-
-#define IDT_TYPE_32_TASK_GATE 5u
-#define IDT_TYPE_16_INT_GATE 6u
-#define IDT_TYPE_16_TRAP_GATE 7u
-#define IDT_TYPE_32_INT_GATE 14u
-#define IDT_TYPE_32_TRAP_GATE 15u
 
 #endif // _IDT_H_INCLUDED_

83c84014696690cd955fdbd7ee27e484f2c4c60a
Collin Baker <chbaker0@gmail.com>
2017-08-06 15:42:01 -0700

    Add rust part of kernel.

---
+++ rustsrc/src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(lang_items)]
+#![no_std]
+
+#[lang="panic_fmt"]
+extern fn panic_fmt(_: ::core::fmt::Arguments, _: &'static str, _: u32) -> ! {
+    loop { }
+}
+
+#[no_mangle]
+pub extern fn rustmain() {
+    loop { }
+}

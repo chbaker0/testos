cc3cbd021a725e614c4628226601453c0ed0ace6
Collin Baker <chbaker0@gmail.com>
2017-09-10 20:27:00 -0400

    Don't allocate memory where the kernel is.

--- rustsrc/src/mm.rs
+++ rustsrc/src/mm.rs
@@ -1,3 +1,4 @@
+use core::cmp;
 use core::mem::uninitialized;
 
 const PAGE_ORDER: u32 = 12;
@@ -10,7 +11,7 @@ pub const PAGE_SIZE: usize = 1 << (PAGE_ORDER);
  * any sane configuration.
  */
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum MemoryStatus {
     Unknown = 0, // Memory is unvailable because it is of unknown
                  // type.
@@ -71,17 +72,27 @@ fn align_address(address: u64, order: u32) -> u64 {
     new_address
 }
 
+
+fn next_entry(mem_map: &MemoryMap, ndx: usize) -> usize {
+    let mut i = ndx;
+    while mem_map.entries[i].status != MemoryStatus::Available {
+        i += 1;
+    }
+    i
+}
+
 impl<'a> FrameAllocator<'a> {
     fn new(mem_map: &'a MemoryMap) -> FrameAllocator<'a> {
         assert!(mem_map.num_entries > 0);
-        let first = mem_map.entries[0];
-        let base = align_address(first.base, PAGE_ORDER);
+        let first = next_entry(&mem_map, 0);
+        let entry = mem_map.entries[first];
+        let base = align_address(entry.base, PAGE_ORDER);
         assert!(base <= usize::max_value() as u64);
-        assert!(base < first.base + first.length);
+        assert!(base < entry.base + entry.length);
         FrameAllocator {
             cur_addr: base as usize,
             mem_map: mem_map,
-            cur_map_entry: 0,
+            cur_map_entry: first,
         }
     }
 
@@ -92,7 +103,7 @@ impl<'a> FrameAllocator<'a> {
         let map_entry = self.mem_map.entries[self.cur_map_entry];
         let next_addr = self.cur_addr + PAGE_SIZE;
         if (next_addr as u64) > map_entry.base + map_entry.length {
-            self.cur_map_entry += 1;
+            self.cur_map_entry = next_entry(&self.mem_map, self.cur_map_entry+1);
             if self.cur_map_entry >= self.mem_map.num_entries {
                 panic!("Out of physical memory.");
             }
@@ -122,23 +133,40 @@ pub fn init(mbinfo: &::multiboot::Info) {
         panic!("init called when already initialized.");
     }
 
-    // Copy multiboot memory map.
-    let mut i = 0;
+    let (_, kernel_end) = {
+        let symtab_info = ::multiboot::get_section_header_table_info(mbinfo);
+        (0..symtab_info.entry_count)
+            .map(|ndx| unsafe { ::elf::get_section_header(symtab_info.addr, symtab_info.entry_size, ndx) })
+            .map(|header| (header.addr as u64, (header.addr + header.size) as u64))
+            .filter(|&(lower, upper)| upper - lower > 0)
+            .fold((u64::max_value(), 0), |(a, b), (c, d)| (cmp::min(a, c), cmp::max(b, d)))
+    };
+
+
+    // Set up memory map.
+    let memory_map = unsafe { &mut MEMORY_MAP };
+
+    // We only consider memory above 1 MiB, and the first memory here will be the kernel.
+    memory_map.entries[0] = MemoryMapEntry {
+        base: 0x100000,
+        length: kernel_end - 0x100000,
+        status: MemoryStatus::Kernel,
+    };
+
+    let mut i = 1;
     for e in ::multiboot::get_memory_map_iterator(mbinfo) {
-        if e.base_addr >= 0x100000 {
-            unsafe {
-                MEMORY_MAP.entries[i] = MemoryMapEntry {
-                    base: e.base_addr,
-                    length: e.length,
-                    status: MemoryStatus::Available,
-                };
-            }
-            i += 1;
-        }
+        if e.base_addr + e.length < kernel_end { continue; }
+
+        let new_base = cmp::max(kernel_end, e.base_addr);
+        let new_length = e.length - (new_base - e.base_addr);
+        memory_map.entries[i] = MemoryMapEntry {
+            base: new_base,
+            length: new_length,
+            status: MemoryStatus::Available,
+        };
+        i += 1;
     }
-    unsafe {
-        MEMORY_MAP.num_entries = i;
-    }
+    memory_map.num_entries = i;
 
     // Initialize frame allocator.
     unsafe {

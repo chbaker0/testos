4a4eda1a0dbaf85097661be56ff0004cc7b2de5d
Collin Baker <chbaker0@gmail.com>
2017-09-13 19:05:26 -0400

    Copy code from kernel.

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -1,16 +1,82 @@
+#![feature(const_fn)]
 #![feature(lang_items)]
 #![no_std]
 
 extern crate rlibc;
 extern crate shared;
 
+mod terminal;
+mod vga;
+
+use core::cell;
+use core::fmt::write;
 use core::mem::size_of;
+use core::ops::DerefMut;
 use core::slice::from_raw_parts;
+use core::str::from_utf8;
 use shared::*;
+
+static mut TERMBUF: cell::RefCell<terminal::Buffer> = cell::RefCell::new(terminal::Buffer::new());
+
+fn log_terminal(s: &str)
+{
+    // Currently only one thread exists, so this is safe.
+    unsafe {
+        let mut termbuf = TERMBUF.borrow_mut();
+        termbuf.write_line(s);
+        vga::display_terminal(termbuf.deref_mut());
+    }
+}
+
+struct BufWriter {
+    buffer: [u8; 80],
+    ndx: usize,
+}
+
+impl BufWriter {
+    fn new() -> BufWriter {
+        BufWriter {
+            buffer: [0; 80],
+            ndx: 0,
+        }
+    }
+}
+
+impl core::fmt::Write for BufWriter {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let trunc = s.bytes().take(80-self.ndx);
+        for c in trunc {
+            self.buffer[self.ndx] = c;
+            self.ndx += 1;
+        }
+        Ok(())
+    }
+}
+
+fn write_terminal(args: core::fmt::Arguments) {
+    let mut buf_writer = BufWriter::new();
+    write(&mut buf_writer, args);
+    buf_writer.buffer[79] = 0;
+
+    match from_utf8(&buf_writer.buffer) {
+        Ok(s) => log_terminal(s),
+        Err(_) => panic!(),
+    }
+}
 
 #[lang="panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt(_: ::core::fmt::Arguments, _: &'static str, _:u32) -> ! {
+pub extern fn panic_fmt(panic_args: ::core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    let mut buf_writer = BufWriter::new();
+    write(&mut buf_writer, format_args!("Panic in {} at line {}: ", file, line));
+    write(&mut buf_writer, panic_args);
+    buf_writer.buffer[79] = 0;
+
+    match from_utf8(&buf_writer.buffer) {
+        Ok(s) => log_terminal(s),
+        Err(_) => (), // We're already panicking, there's nothing else to do.
+    }
+
     loop { }
 }
 

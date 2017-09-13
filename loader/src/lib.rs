67d57871940d57e0a69c8bdef552fbdae385b485
Collin Baker <chbaker0@gmail.com>
2017-09-13 14:14:13 -0400

    Get module info from multiboot.

--- loader/src/lib.rs
+++ loader/src/lib.rs
@@ -4,13 +4,43 @@
 extern crate rlibc;
 extern crate shared;
 
+use core::slice::from_raw_parts;
+use shared::*;
+
 #[lang="panic_fmt"]
 #[no_mangle]
 pub extern fn panic_fmt(_: ::core::fmt::Arguments, _: &'static str, _:u32) -> ! {
     loop { }
 }
 
+#[repr(C, packed)]
+struct ModuleRaw {
+    start: u32,
+    end: u32,
+    string: u32,
+    reserved: u32,
+}
+
+struct Module {
+    data: &'static [u8],
+}
+
+impl Module {
+    fn from_raw(mr: &ModuleRaw) -> Self {
+        let startp = mr.start as *const u8;
+        let len = (mr.end - mr.start) as usize;
+        Module {
+            data: unsafe { from_raw_parts(startp, len) },
+        }
+    }
+}
+
 #[no_mangle]
-pub extern fn loader_entry() {
+pub extern fn loader_entry(mbinfop: *const multiboot::Info) {
+    let mbinfo = unsafe { &*mbinfop };
+    let mod_raw_entries = unsafe {
+        from_raw_parts(mbinfo.mods_addr as *const ModuleRaw, mbinfo.mods_count as usize)
+    };
+    let mod_entries = mod_raw_entries.into_iter().map(Module::from_raw);
     loop { }
 }

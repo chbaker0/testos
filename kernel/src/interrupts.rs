5d6d6cc1c9a7c3cca4e559b65050d17f33f686cb
Collin Baker <chbaker0@gmail.com>
2017-10-10 20:36:26 -0400

    Add trivial page fault handler.

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -3,10 +3,17 @@ use x86_64::structures::idt;
 lazy_static! {
     static ref IDT: idt::Idt = {
         let mut idt = idt::Idt::new();
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
 
 pub fn init() {
     IDT.load();
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    _: &mut idt::ExceptionStackFrame,
+    _: idt::PageFaultErrorCode) {
+    panic!("Page fault occurred.");
 }

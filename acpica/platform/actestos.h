0f20e20aa041da5b9a48d113d4080c5c25397e85
Collin Baker <chbaker0@gmail.com>
2017-12-04 13:15:04 -0500

    Add ACPICA sources.

---
+++ acpica/platform/actestos.h
@@ -0,0 +1,11 @@
+#ifndef __ACTESTOS_H__
+#define __ACTESTOS_H__
+
+#define ACPI_MACHINE_WIDTH 64
+#define COMPILER_DEPENDENT_INT64 long
+#define COMPILER_DEPENDENT_UINT64 unsigned long
+
+#define ACPI_DISASSEMBLER
+#define ACPI_DEBUGGER
+
+#endif // __ACTESTOS_H__

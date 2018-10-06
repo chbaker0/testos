86a69a21cfd67a05047b3d18bdc12119dc7e3d68
Collin Baker <chbaker0@gmail.com>
2018-10-06 12:18:08 -0700

    Update dependencies

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,9 +1,8 @@
-use x86_64;
 use x86_64::structures::idt;
 
 lazy_static! {
-    static ref IDT: idt::Idt = {
-        let mut idt = idt::Idt::new();
+    static ref IDT: idt::InterruptDescriptorTable = {
+        let mut idt = idt::InterruptDescriptorTable::new();
         idt.page_fault.set_handler_fn(page_fault_handler);
         idt.double_fault.set_handler_fn(double_fault_handler);
         idt
@@ -17,7 +16,8 @@ pub fn init() {
 extern "x86-interrupt" fn page_fault_handler(
     _: &mut idt::ExceptionStackFrame,
     error_code: idt::PageFaultErrorCode) {
-    let addr = x86_64::registers::control_regs::cr2();
+    let addr: u64;
+    unsafe { asm!("mov %cr2, $0" : "=r"(addr)); }
     panic!("Page fault occurred on address {:x}: {:?}", addr, error_code);
 }
 

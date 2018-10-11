15de90112fe3b975a9faee4b659e4d87912850e2
Collin Baker <chbaker0@gmail.com>
2018-10-10 18:13:17 -0700

    Strip debug symbols from main image.

--- build.sh
+++ build.sh
@@ -13,7 +13,13 @@ cd ..
 nasm -f elf32 -o out/boot.nasm.o boot.nasm
 
 x86_64-elf-ld -g -T kernel.ld -z max-page-size=0x1000 --gc-sections -o out/kernel kernel/target/x86_64-unknown-none/debug/libkernel.a
+objcopy --only-keep-debug out/kernel out/kernel.sym
+objcopy --strip-debug out/kernel
+
+
 i686-elf-ld -g -T loader.ld -z max-page-size=0x1000 --gc-sections -o out/loader out/boot.nasm.o loader/target/i686-unknown-none/debug/libloader.a
+objcopy --only-keep-debug out/loader out/loader.sym
+objcopy --strip-debug out/loader
 
 mkdir -p out/iso/boot/grub
 cp grub.cfg out/iso/boot/grub

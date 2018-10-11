eb66ed617ab40aa2eca58f29f4c01396506dcced
Collin Baker <chbaker0@gmail.com>
2018-10-10 21:07:28 -0700

    Use native ld instead of cross-compiled ones.

--- build.sh
+++ build.sh
@@ -16,12 +16,12 @@ cd ..
 nasm -f elf32 -o out/boot.nasm.o boot.nasm
 
 # Link kernel image from cargo-built image using linker script kernel.ld.
-x86_64-elf-ld -g -T kernel.ld -z max-page-size=0x1000 --gc-sections -o out/kernel kernel/target/x86_64-unknown-none/debug/libkernel.a
+ld -melf_x86_64 -g -T kernel.ld -z max-page-size=0x1000 --gc-sections -o out/kernel kernel/target/x86_64-unknown-none/debug/libkernel.a
 objcopy --only-keep-debug out/kernel out/kernel.sym
 objcopy --strip-debug out/kernel
 
 # Link loader image from cargo-built image using linker script loader.ld.
-i686-elf-ld -g -T loader.ld -z max-page-size=0x1000 --gc-sections -o out/loader out/boot.nasm.o loader/target/i686-unknown-none/debug/libloader.a
+ld -melf_i386 -g -T loader.ld -z max-page-size=0x1000 --gc-sections -o out/loader out/boot.nasm.o loader/target/i686-unknown-none/debug/libloader.a
 objcopy --only-keep-debug out/loader out/loader.sym
 objcopy --strip-debug out/loader
 

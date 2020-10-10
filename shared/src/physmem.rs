57d65c8632fbbed6f3f80aafa9de1ec9ab8dd0a3
Collin <chbaker0@gmail.com>
2020-10-09 22:42:01 -0700

    Implement rest of BumpAllocator

--- shared/src/physmem.rs
+++ shared/src/physmem.rs
@@ -3,6 +3,7 @@ use core::iter::IntoIterator;
 
 use arrayvec::ArrayVec;
 use itertools::put_back;
+use itertools::structs::PutBack;
 
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Debug, Hash)]
 pub struct Address(u64);
@@ -33,13 +34,13 @@ impl Address {
 
     /// Returns the first address below `self` that is aligned to `alignment`,
     /// which must be a power of two.
-    pub fn align_down(&self, alignment: usize) -> Address {
+    pub fn align_down(&self, alignment: u64) -> Address {
         Self::from_raw(align_u64_down(self.as_raw(), alignment))
     }
 
     /// Returns the first address above `self` that is aligned to `alignment`,
     /// which must be a power of two.
-    pub fn align_up(&self, alignment: usize) -> Address {
+    pub fn align_up(&self, alignment: u64) -> Address {
         Self::from_raw(align_u64_up(self.as_raw(), alignment))
     }
 }
@@ -67,13 +68,13 @@ impl Length {
 
     /// Returns the first length lesser than `self` that is aligned to `alignment`,
     /// which must be a power of two.
-    pub fn align_down(&self, alignment: usize) -> Length {
+    pub fn align_down(&self, alignment: u64) -> Length {
         Length::from_raw(align_u64_down(self.as_raw(), alignment))
     }
 
     /// Returns the first length greater than `self` that is aligned to `alignment`,
     /// which must be a power of two.
-    pub fn align_up(&self, alignment: usize) -> Length {
+    pub fn align_up(&self, alignment: u64) -> Length {
         Length::from_raw(align_u64_up(self.as_raw(), alignment))
     }
 }
@@ -86,11 +87,17 @@ pub struct Extent {
 
 impl Extent {
     pub fn new(address: Address, length: Length) -> Extent {
-        assert_ne!(0, length.as_raw());
-        assert!(length.as_raw() <= u64::MAX - address.as_raw());
-        Extent {
-            address: address,
-            length: length,
+        Self::new_checked(address, length).unwrap()
+    }
+
+    pub fn new_checked(address: Address, length: Length) -> Option<Extent> {
+        if length.as_raw() == 0 || length.as_raw() > u64::MAX - address.as_raw() {
+            None
+        } else {
+            Some(Extent {
+                address: address,
+                length: length,
+            })
         }
     }
 
@@ -189,7 +196,7 @@ impl Extent {
     /// Returns the largest extent completely contained in `self` whose start
     /// and end addresses are aligned to `alignment`. `alignment` must be a
     /// power of two.
-    pub fn shrink_to_alignment(&self, alignment: usize) -> Option<Extent> {
+    pub fn shrink_to_alignment(&self, alignment: u64) -> Option<Extent> {
         let start_address = self.address.align_up(alignment);
         let end_address = self.end_address().align_down(alignment);
         if end_address <= start_address {
@@ -219,6 +226,13 @@ impl Map {
     pub fn entries(&self) -> &[MapEntry] {
         &self.entries
     }
+
+    pub fn iter_type<'a>(&'a self, mem_type: MemoryType) -> impl Iterator<Item = Extent> + 'a {
+        self.entries
+            .iter()
+            .filter(move |e| e.mem_type == mem_type)
+            .map(|e| e.extent)
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -244,105 +258,181 @@ pub enum MemoryType {
 /// Allocates physical pages of a given size from the lowest to highest address.
 /// Does not support freeing.
 pub struct BumpAllocator {
+    // This is in reverse order.
     free: ArrayVec<[Extent; 128]>,
-    page_size: usize,
+    // This is the base 2 log of the page size.
+    page_size_log2: usize,
 }
 
 impl BumpAllocator {
     /// Allocates from the available memory in `map` after removing specified
-    /// regions in `reserved`. Allocations are multiples of `page_size`, which
-    /// must be a power of two.
+    /// regions in `holes`. Allocations are multiples of `page_size`, which must
+    /// be a power of two.
     ///
-    /// `reserved` must be sorted.
+    /// `holes` must be sorted.
     ///
-    /// Addresses in `map` and `reserved` do not need to be aligned. However,
-    /// all returned allocations will be aligned to `page_size`.
+    /// Addresses in `map` and `holes` do not need to be aligned. However, all
+    /// returned allocations will be aligned to `page_size`.
     pub fn new<T: IntoIterator<Item = Extent>>(
-        page_size: usize,
+        page_size: u64,
         map: &Map,
-        reserved: T,
+        holes: T,
     ) -> BumpAllocator {
-        let mut map_iter = put_back(
-            map.entries()
-                .iter()
-                .filter(|e| e.mem_type == MemoryType::Available)
-                .map(|e| e.extent),
-        );
-        let mut reserved_iter = put_back(reserved);
+        // Make sure `page_size` is a power of two.
+        assert_eq!(page_size.count_ones(), 1);
 
-        let mut free = ArrayVec::<[Extent; 128]>::new();
+        // The base 2 log of a power of 2 is simply the number of trailing
+        // zeros.
+        let page_size_log2 = page_size.trailing_zeros() as usize;
 
-        // Insert free blocks into `free`, removing entries in `reserved`.
-        while let Some(ext) = map_iter.next() {
-            // Remove holes completely before `ext`; they can be ignored.
-            while let Some(hole) = reserved_iter.next() {
-                if hole.last_address() >= ext.address() {
-                    reserved_iter.put_back(hole);
-                    break;
-                }
-            }
+        let map_iter = map.iter_type(MemoryType::Available);
 
-            // Get the next hole.
-            let hole = match reserved_iter.next() {
-                Some(ext) => ext,
-                None => break,
-            };
-
-            // If `hole` is completely after `ext`, we can put `ext` in `free`.
-            // However, we must retain `hole` in case it intersects with a
-            // future `ext`.
-            if ext.last_address() < hole.address() {
-                free.push(ext);
-                reserved_iter.put_back(hole);
-                continue;
-            }
-
-            // We now know `hole` intersects `ext`: it is not completely before
-            // `ext`, nor completely after `ext`. Get both sides of the
-            // difference of `hole` from `ext`.
-            let maybe_left = ext.left_difference(&hole);
-            let maybe_right = ext.right_difference(&hole);
-
-            // There are no holes left that may intersect `left`, so go ahead
-            // and insert it.
-            if let Some(left) = maybe_left {
-                free.push(left);
-            }
-
-            if let Some(right) = maybe_right {
-                // There may be another hole that will intersect `right`. Put it
-                // back for next iteration. We can throw away `hole` though.
-                map_iter.put_back(right);
-            } else {
-                // `hole` may extend beyond `ext`. Put it back for next
-                // iteration.
-                reserved_iter.put_back(hole);
-            }
-        }
-
-        // `reserved_iter` is done. Copy the remaining entries directly to `free`.
-        while let Some(ext) = map_iter.next() {
-            free.push(ext);
-        }
+        let mut free: ArrayVec<[Extent; 128]> = reserve(map_iter, holes)
+            .flat_map(|e| e.shrink_to_alignment(page_size))
+            .collect();
+        free = free.iter().rev().copied().collect();
 
         BumpAllocator {
             free: free,
-            page_size: page_size,
+            page_size_log2: page_size_log2,
         }
+    }
+
+    pub fn allocate_pages(&mut self, pages: u64) -> Address {
+        // Check that pages * (2^page_size_log2) <= u64::MAX without overflow.
+        assert!(pages as u64 <= (u64::MAX >> self.page_size_log2));
+        self.allocate_impl(Length::from_raw(pages << self.page_size_log2))
+    }
+
+    pub fn allocate(&mut self, length: Length) -> Address {
+        self.allocate_impl(length.align_up(1 << self.page_size_log2))
+    }
+
+    // `alloc_length` must be aligned to the page size.
+    fn allocate_impl(&mut self, alloc_length: Length) -> Address {
+        assert!(alloc_length.as_raw().trailing_zeros() >= self.page_size_log2 as u32);
+
+        // The last element of `self.free` contains the first available block.
+        let mut block;
+        loop {
+            block = match self.free.pop() {
+                Some(block) => block,
+                None => panic!("out of memory"),
+            };
+
+            // Use this block if it's big enough.
+            if alloc_length <= block.length() {
+                break;
+            }
+
+            // Discard the block. We're just a bump allocator, after all.
+        }
+
+        let alloc_address = block.address();
+
+        let maybe_remainder = Extent::new_checked(
+            block.address().offset_by(&alloc_length),
+            block.length().subtract(&alloc_length),
+        );
+
+        if let Some(remainder) = maybe_remainder {
+            self.free.push(remainder);
+        }
+
+        alloc_address
+    }
+}
+
+/// Removes specified regions from a list of blocks of memory.
+///
+/// Given `blocks`, a list of available memory, removes the regions specified in
+/// `holes` and returns the remaining free memory. This may involve splitting
+/// extents in `blocks`. The resulting list may be larger than `blocks`.
+///
+/// Both lists must be sorted by start address and non-overlapping.
+fn reserve<T: IntoIterator<Item = Extent>, U: IntoIterator<Item = Extent>>(
+    blocks: T,
+    holes: U,
+) -> impl Iterator<Item = Extent> {
+    ReserveIter {
+        blocks: put_back(blocks),
+        holes: put_back(holes),
+    }
+    .flatten()
+}
+
+struct ReserveIter<I1: Iterator, I2: Iterator> {
+    blocks: PutBack<I1>,
+    holes: PutBack<I2>,
+}
+
+impl<I1, I2> Iterator for ReserveIter<I1, I2>
+where
+    I1: Iterator<Item = Extent>,
+    I2: Iterator<Item = Extent>,
+{
+    type Item = Option<Extent>;
+
+    fn next(&mut self) -> Option<Option<Extent>> {
+        let block = self.blocks.next()?;
+
+        // Remove holes completely before `ext`; they can be ignored.
+        while let Some(hole) = self.holes.next() {
+            if hole.last_address() >= block.address() {
+                self.holes.put_back(hole);
+                break;
+            }
+        }
+
+        // Get the next hole. If there are none left, we can simply return
+        // `block`.
+        let hole = match self.holes.next() {
+            Some(hole) => hole,
+            None => return Some(Some(block)),
+        };
+
+        // If `hole` is completely after `block`, we can return `block`.
+        // However, we must retain `hole` in case it intersects with a future
+        // `block`.
+        if block.last_address() < hole.address() {
+            self.holes.put_back(hole);
+            return Some(Some(block));
+        }
+
+        // We now know `hole` intersects `ext`: it is not completely before
+        // `ext`, nor completely after `ext`. Get both sides of the
+        // difference of `hole` from `ext`.
+        assert!(block.has_overlap(&hole));
+        let maybe_left = block.left_difference(&hole);
+        let maybe_right = block.right_difference(&hole);
+
+        if let Some(right) = maybe_right {
+            // There may be another hole that will intersect `right`. Put it
+            // back for next iteration. We can throw away `hole` though.
+            self.blocks.put_back(right);
+        } else {
+            // `hole` may extend beyond `block`. Put it back for next iteration.
+            self.holes.put_back(hole);
+        }
+
+        // There are no holes left that may intersect `maybe_left`, if it
+        // exists. If `block` is none our caller will ignore the `Some(None)`
+        // return value.
+        Some(maybe_left)
     }
 }
 
 /// Given power-of-two `alignment`, returns the largest value below `x` aligned
 /// to `alignment`
-const fn align_u64_down(x: u64, alignment: usize) -> u64 {
-    let mask = !(alignment as u64 - 1);
+const fn align_u64_down(x: u64, alignment: u64) -> u64 {
+    let mask = !(alignment - 1);
     x & mask
 }
 
 /// Given power-of-two `alignment`, returns the smallest value above `x` aligned
 /// to `alignment`
-const fn align_u64_up(x: u64, alignment: usize) -> u64 {
-    align_u64_down(x + (alignment - 1) as u64, alignment)
+const fn align_u64_up(x: u64, alignment: u64) -> u64 {
+    align_u64_down(x + (alignment - 1), alignment)
 }
 
 #[cfg(test)]
@@ -529,6 +619,144 @@ mod tests {
         assert_eq!(
             Extent::from_raw(10, 10).right_difference(&Extent::from_raw(8, 14)),
             None
+        );
+    }
+
+    #[test]
+    fn reserve_no_holes() {
+        let result: Vec<_> = reserve(
+            [Extent::from_raw(1, 4), Extent::from_raw(10, 4)]
+                .iter()
+                .copied(),
+            [].iter().copied(),
+        )
+        .collect();
+        assert_eq!(result, [Extent::from_raw(1, 4), Extent::from_raw(10, 4)]);
+
+        let result: Vec<_> = reserve([].iter().copied(), [].iter().copied()).collect();
+        assert_eq!(result, []);
+    }
+
+    #[test]
+    fn reserve_no_blocks() {
+        let result: Vec<_> = reserve(
+            [].iter().copied(),
+            [Extent::from_raw(5, 5), Extent::from_raw(15, 5)]
+                .iter()
+                .copied(),
+        )
+        .collect();
+        assert_eq!(result, []);
+    }
+
+    #[test]
+    fn reserve_big() {
+        let result: Vec<_> = reserve(
+            [
+                Extent::from_raw(0, 5),
+                Extent::from_raw(7, 2),
+                Extent::from_raw(10, 10),
+                Extent::from_raw(25, 5),
+                Extent::from_raw(35, 10),
+            ]
+            .iter()
+            .copied(),
+            [
+                Extent::from_raw(0, 3),
+                Extent::from_raw(6, 4),
+                Extent::from_raw(12, 4),
+                Extent::from_raw(27, 3),
+                Extent::from_raw(32, 4),
+                Extent::from_raw(44, 2),
+            ]
+            .iter()
+            .copied(),
+        )
+        .collect();
+
+        assert_eq!(
+            result,
+            [
+                Extent::from_raw(3, 2),
+                Extent::from_raw(10, 2),
+                Extent::from_raw(16, 4),
+                Extent::from_raw(25, 2),
+                Extent::from_raw(36, 8)
+            ]
+        );
+    }
+
+    #[test]
+    fn reserve_multiple_holes_in_one_block() {
+        let result: Vec<_> = reserve(
+            [Extent::from_raw(10, 20)].iter().copied(),
+            [
+                Extent::from_raw(8, 4),
+                Extent::from_raw(15, 5),
+                Extent::from_raw(22, 2),
+                Extent::from_raw(28, 10),
+            ]
+            .iter()
+            .copied(),
+        )
+        .collect();
+
+        assert_eq!(
+            result,
+            [
+                Extent::from_raw(12, 3),
+                Extent::from_raw(20, 2),
+                Extent::from_raw(24, 4)
+            ]
+        );
+    }
+
+    #[test]
+    fn reserve_multiple_blocks_in_one_hole() {
+        let result: Vec<_> = reserve(
+            [
+                Extent::from_raw(4, 2),
+                Extent::from_raw(10, 5),
+                Extent::from_raw(20, 10),
+                Extent::from_raw(38, 4),
+            ]
+            .iter()
+            .copied(),
+            [Extent::from_raw(5, 35)].iter().copied(),
+        )
+        .collect();
+
+        assert_eq!(result, [Extent::from_raw(4, 1), Extent::from_raw(40, 2)]);
+    }
+
+    #[test]
+    fn bump_allocator() {
+        let page_size = 4096;
+
+        let map = Map::from_entries(
+            [MapEntry {
+                extent: Extent::from_raw(0, 131072),
+                mem_type: MemoryType::Available,
+            }]
+            .iter()
+            .copied(),
+        );
+
+        let holes = [Extent::from_raw(4095, 4098)];
+
+        let mut allocator = BumpAllocator::new(page_size, &map, holes.iter().copied());
+
+        assert_eq!(
+            allocator.allocate_pages(2),
+            Address::from_raw(page_size * 3)
+        );
+        assert_eq!(
+            allocator.allocate(Length::from_raw(20)),
+            Address::from_raw(page_size * 5)
+        );
+        assert_eq!(
+            allocator.allocate_pages(1),
+            Address::from_raw(page_size * 6)
         );
     }
 }

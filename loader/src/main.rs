3cde5990685787147ea7caac3b28bdb24c2c3bb4
Collin <chbaker0@gmail.com>
2020-10-10 12:14:24 -0700

    Compute kernel load size from program headers

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -6,9 +6,12 @@ mod multiboot;
 use core::fmt::Write;
 use core::panic::PanicInfo;
 
+use xmas_elf::program;
 use xmas_elf::ElfFile;
 
 use shared::physmem;
+
+const PAGE_SIZE: u64 = 4096;
 
 const VMEM: *mut u8 = 0xb8000 as *mut u8;
 
@@ -39,13 +42,13 @@ pub extern "C" fn loader_main(boot_info_ptr: *const multiboot::BootInfo) -> ! {
     }
 
     // Assume we won't overwrite the module.
-    let kernel_data = unsafe { multiboot::get_first_module(boot_info) };
+    let kernel_image = unsafe { multiboot::get_first_module(boot_info) };
 
     writeln!(&mut writer, "\n").unwrap();
-    writeln!(&mut writer, "Kernel addr: {:p}", kernel_data.as_ptr()).unwrap();
-    writeln!(&mut writer, "Kernel size: {}", kernel_data.len()).unwrap();
+    writeln!(&mut writer, "Kernel addr: {:p}", kernel_image.as_ptr()).unwrap();
+    writeln!(&mut writer, "Kernel size: {}", kernel_image.len()).unwrap();
 
-    let kernel_elf = ElfFile::new(kernel_data).unwrap();
+    let kernel_elf = ElfFile::new(kernel_image).unwrap();
 
     write!(&mut writer, "Kernel sections:").unwrap();
     for section in kernel_elf.section_iter() {
@@ -63,8 +66,8 @@ pub extern "C" fn loader_main(boot_info_ptr: *const multiboot::BootInfo) -> ! {
     // loading the kernel.
     let loader_extent = get_loader_extent();
     let kernel_extent = physmem::Extent {
-        address: physmem::Address::from_raw(kernel_data.as_ptr() as u64),
-        length: physmem::Length::from_raw(kernel_data.len() as u64),
+        address: physmem::Address::from_raw(kernel_image.as_ptr() as u64),
+        length: physmem::Length::from_raw(kernel_image.len() as u64),
     };
 
     writeln!(&mut writer, "Loader extent: {:?}", get_loader_extent()).unwrap();
@@ -79,12 +82,12 @@ pub extern "C" fn loader_main(boot_info_ptr: *const multiboot::BootInfo) -> ! {
     reserved_extents.sort_unstable_by_key(|e| e.address());
 
     let mut allocator =
-        physmem::BumpAllocator::new(4096, &memory_map, reserved_extents.iter().copied());
+        physmem::BumpAllocator::new(PAGE_SIZE, &memory_map, reserved_extents.iter().copied());
 
     // This is where we'll copy the kernel sections.
     let kernel_target = physmem::Extent {
         address: allocator.allocate(kernel_extent.length()),
-        length: kernel_extent.length(),
+        length: get_kernel_load_size(&kernel_elf),
     };
 
     writeln!(&mut writer, "Kernel load target: {:?}", kernel_target).unwrap();
@@ -128,6 +131,24 @@ fn clear_screen() {
             *VMEM.offset(2 * i) = ' ' as u8;
         }
     }
+}
+
+fn get_kernel_load_size(kernel_image: &ElfFile) -> physmem::Length {
+    use program::Type;
+
+    let mut length = physmem::Length::from_raw(0);
+
+    for pg_header in kernel_image.program_iter() {
+        let segment_size = match pg_header.get_type().unwrap() {
+            Type::Null => 0,
+            Type::Load => pg_header.mem_size(),
+            unsupported => panic!("unsupported section {:?}", unsupported),
+        };
+
+        length = length.add(&physmem::Length::from_raw(segment_size).align_up(PAGE_SIZE));
+    }
+
+    length
 }
 
 fn get_loader_extent() -> physmem::Extent {
